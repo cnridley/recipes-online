968b275a1970ddb369614931e1c521184ebb6fb5
cnridley <cnridley1@gmail.com>
2020-08-31 22:31:05 +0000

    wired edit button to mongo db

--- app.py
+++ app.py
@@ -115,6 +115,14 @@ def add_recipe():
     return render_template("add_recipes.html", categories=categories)
 
 
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.Recipe.find_one({"_id": ObjectId(recipe_id)})
+
+    categories = mongo.db.Category.find().sort("category_name", 1)
+    return render_template("edit_recipes.html", recipe=recipe, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
