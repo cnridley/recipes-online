9eae099cc814366ec4208d3667b2efb137a77fa9
cnridley <cnridley1@gmail.com>
2020-09-02 14:43:40 +0000

    added favourite toggle

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,4 +2,10 @@
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
+    $('#favouriteBtn').click(function(){
+        $('#favouriteBtn').css('backgroundColor', '#f44336');
+        $('#favouriteBtn').css('color', 'black');
+    });
   });
+
+ 
